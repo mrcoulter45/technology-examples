c23f0ef567238dc31707017488db73db1a94a5d2
Michael Coulter <michael.coulter@arm.com>
2019-11-11 01:57:55 -0600

    Add to React example "Composing Components" -> "Single Source of Truth" 11-11-2019

--- React/counter-app/src/components/counter.jsx
+++ React/counter-app/src/components/counter.jsx
@@ -2,30 +2,32 @@ import React, { Component } from 'react'; // React object is never used explicit
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.counter.value, // using 'value' prop passed from parent component
     tags: ['tag1','tag2','tag3'],
     imageUrl: "https://picsum.photos/200"
   };
 
-  constructor() {
-    super(); // 
-    this.handleIncrement = this.handleIncrement.bind(this);
-  }
+  // props must be passed to constructor if constructor is used because the constructor for a React component is called before it is mounted. When implementing the constructor for a React.Component subclass, you should call super(props) before any other statement. Otherwise, this.props will be undefined in the constructor, which will cause bugs.
+  // constructor(props) {
+  //   super(props);
+    // this.handleIncrement = this.handleIncrement.bind(this); // needed if event handler is being called by reference instead of inline function
+  // }
 
   formatCount() {
-    return this.state.count === 0 ? "Zero" : this.state.count;
+    return this.state.value === 0 ? "Zero" : this.state.value;
   }
 
   // used for dynamic class assignment
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   // Conditional rendering
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    // rendering lists
     return (
       <ul>
         { this.state.tags.map(tag => <li key={tag}>{tag}</li>) } {/* map function creates a new array with the results of calling a provided function on every element in the calling array */}
@@ -33,25 +35,31 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement(count) {
-    console.log(count);
-    this.setState({count: this.state.count + 1}); // this function is inherited from 'Component' parent class
+  // event handler for 'Increment' button
+  handleIncrement(value) {
+    this.setState({value: this.state.value + 1}); // this function 'setState' is inherited from the 'Component' parent class. Used to update component state
   }
 
   render() {
     return (
-      <React.Fragment> {/*can be used instead of a div so that an unnessary div is not rendered in the DOM*/}
-        <img src={this.state.imageUrl} alt=""/>
+      <div> {/* A 'React.Fragment' can be used instead of a div so that an unnessary div is not rendered in the DOM, however, elements will be in the same row */}
+        {/*<img src={this.state.imageUrl} alt=""/>*/}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button 
-          // onClick={this.handleIncrement} // Used if no argument to pass
-          onClick={() => this.handleIncrement(this.state.count)} // Inline function. Used when we need to pass an argument to an event handler (1:12:59 https://www.youtube.com/watch?v=Ke90Tje7VS0)
+          // onClick={this.handleIncrement} // Function reference can be used if no arguments need to be passed
+          onClick={() => this.handleIncrement(this.state.value)} // Inline function. Used when we need to pass an argument to an event handler (1:12:59 https://www.youtube.com/watch?v=Ke90Tje7VS0)
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.renderTags()}
-      </React.Fragment>
+        <button 
+          onClick={() => this.props.onDelete(this.props.counter.id)} // Here, the 'onDelete' prop is a reference to the 'handleDelete' function in 'Counters'.
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+        {/*{this.renderTags()}*/}
+      </div>
     );
   }
 
