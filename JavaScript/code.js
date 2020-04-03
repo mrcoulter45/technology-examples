3af7c4aa7e4896b0aabbb48ca8260a44ab4b3c99
Michael Coulter <michael.coulter@arm.com>
2020-04-02 22:11:09 -0500

    Add info on deep copy

--- JavaScript/code.js
+++ JavaScript/code.js
@@ -1,5 +1,4 @@
 // Object Deep Copy
-
 let obj2 = {a: 'cat', b: {a: 'cat', b: {a: 'cat', b: 'dog', c: 'fish'}, c: 'fish'}, c: 'fish'};
 let obj3 = JSON.parse(JSON.stringify(obj2));
 obj3.a = 'zebra';
@@ -7,8 +6,16 @@ obj3.b.b.b += 'gy';
 console.log(obj2);
 console.log(obj3);
 
-// > Object { a: "cat", b: Object { a: "cat", b: Object { a: "cat", b: "dog", c: "fish" }, c: "fish" }, c: "fish" }
-// > Object { a: "zebra", b: Object { a: "cat", b: Object { a: "cat", b: "doggy", c: "fish" }, c: "fish" }, c: "fish" }
+// > {
+// >   a: 'cat',
+// >   b: { a: 'cat', b: { a: 'cat', b: 'dog', c: 'fish' }, c: 'fish' },
+// >   c: 'fish'
+// > }
+// > {
+// >   a: 'zebra',
+// >   b: { a: 'cat', b: { a: 'cat', b: 'doggy', c: 'fish' }, c: 'fish' },
+// >   c: 'fish'
+// > }
 // Use JSON.parse(JSON.stringify(obj)) for true deep copy
 
 let obj0 = {a: 'cat', b: {a: 'cat', b: {a: 'cat', b: 'dog', c: 'fish'}, c: 'fish'}, c: 'fish'};
@@ -18,9 +25,40 @@ obj1.b.b.b += 'gy';
 console.log(obj0);
 console.log(obj1);
 
-// > Object { a: "cat", b: Object { a: "cat", b: Object { a: "cat", b: "doggy", c: "fish" }, c: "fish" }, c: "fish" }
-// > Object { a: "zebra", b: Object { a: "cat", b: Object { a: "cat", b: "doggy", c: "fish" }, c: "fish" }, c: "fish" }
+// > {
+// >   a: 'cat',
+// >   b: { a: 'cat', b: { a: 'cat', b: 'doggy', c: 'fish' }, c: 'fish' },
+// >   c: 'fish'
+// > }
+// > {
+// >   a: 'zebra',
+// >   b: { a: 'cat', b: { a: 'cat', b: 'doggy', c: 'fish' }, c: 'fish' },
+// >   c: 'fish'
+// > }
 // Spread operator cannot be used for true object deep copy
+
+// Array Deep Copy
+let numbers0 = [1, [2, 3], [3, [4, 5, 6]], 5];
+let numbers1 = [1, 2, 3, 4];
+
+// can only be done when the array consists purely of JSON objects,
+// array cannot contain any functions or React elements
+let numbers2 = JSON.parse(JSON.stringify(numbers0));
+let numbers3 = JSON.parse(JSON.stringify(numbers1));
+numbers2[2][1][2] = 42;
+numbers3[2] = 17;
+
+console.log(numbers0);
+console.log(numbers1);
+console.log(numbers2);
+console.log(numbers3);
+
+// > [ 1, [ 2, 3 ], [ 3, [ 4, 5, 6 ] ], 5 ]
+// > [ 1, 2, 3, 4 ]
+// > [ 1, [ 2, 3 ], [ 3, [ 4, 5, 42 ] ], 5 ]
+// > [ 1, 2, 17, 4 ]
+// Use JSON.parse(JSON.stringify()) for true deep copy, not spread operator
+
 
 // Promises
 var promise0 = new Promise((resolve, reject) => {
